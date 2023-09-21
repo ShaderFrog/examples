a93b6bb02d62e5025570f449088e12c000d10a41
Andrew Ray <me@andrewray.me>
2023-09-20 17:00:32 -0700

    Version 0.0 API example

--- three/index.js
+++ three/index.js
@@ -4,15 +4,9 @@ import * as core from '@shaderfrog/core';
 import { generate } from '@shaderfrog/glsl-parser';
 import { engine, createMaterial } from '@shaderfrog/core/dist/plugins/three';
 
-// TODO: Try putting graph here
-console.log({ core, engine });
-let id = 0;
-const makeId = () => `id_${id++}`;
-
-const outFrom = (node) => node.outputs[0].name;
-const edgeFrom = (fromNode, toId, input, type) =>
-  core.makeEdge(makeId(), fromNode.id, toId, outFrom(fromNode), input, type);
-
+/**
+ * Standard Three.js setup
+ */
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -32,7 +26,8 @@ const helper = new THREE.PointLightHelper(pointLight, 0.1);
 scene.add(pointLight);
 scene.add(helper);
 
-const geometry = new THREE.TorusKnotGeometry(0.6, 0.25, 200, 32);
+// const geometry = new THREE.TorusKnotGeometry(0.6, 0.25, 200, 32);
+const geometry = new THREE.BoxGeometry(1, 1, 1, 32, 32, 32);
 const mesh = new THREE.Mesh(geometry);
 scene.add(mesh);
 
@@ -40,10 +35,38 @@ camera.position.z = 2;
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
+function animate(time) {
+  requestAnimationFrame(animate);
+  controls.update();
+  mesh.rotation.x += 0.005;
+  mesh.rotation.y += 0.005;
+
+  const uniform = mesh.material?.uniforms?.[`start_${fId}`];
+  if (uniform) {
+    uniform.value = new THREE.Vector2(
+      -0.2307 + 0.05 * Math.sin(time * 0.001),
+      0.6923 + 0.05 * Math.cos(time * 0.001)
+    );
+  }
+  renderer.render(scene, camera);
+}
+animate();
+
+/**
+ * Helper functions
+ */
+let id = 0;
+const makeId = () => `id_${id++}`;
+
+const outFrom = (node) => node.outputs[0].name;
+const edgeFrom = (fromNode, toId, input, type) =>
+  core.makeEdge(makeId(), fromNode.id, toId, outFrom(fromNode), input, type);
+
+/**
+ * Build the initial scene "context" to pass to the compiler
+ */
 const ctx = {
   engine: 'three',
-  // TODO: Rename runtime to "engine" and make a new nodes and data top level
-  // key cache (if we keep the material cache) and type it in the graph
   runtime: {
     three: THREE,
     renderer,
@@ -61,6 +84,9 @@ const ctx = {
   debuggingNonsense: {},
 };
 
+/**
+ * Build the foundations of the graph
+ */
 const outputF = core.outputNode(
   makeId(),
   'Output',
@@ -74,6 +100,9 @@ const outputV = core.outputNode(
   'vertex'
 );
 
+/**
+ * Build the Three.js MeshPhysicalMaterial graph node
+ */
 const physicalGroupId = makeId();
 const physicalF = engine.constructors.physical(
   makeId(),
@@ -93,35 +122,126 @@ const physicalV = engine.constructors.physical(
   physicalF.id
 );
 
+/**
+ * Create the Julia shader nodes
+ */
+const makeJuliaF = (id) =>
+  core.sourceNode(
+    id,
+    'Julia',
+    {},
+    {
+      version: 2,
+      preprocess: true,
+      strategies: [core.uniformStrategy()],
+    },
+    `precision highp float;
+precision highp int;
+
+uniform vec2 start;
+uniform int iter;
+uniform vec3 fractal_color;
+uniform float time;
+varying vec2 vUv;
+
+void main() {
+    vec2 z;
+    z.x = 3.0 * (vUv.x - .5);
+    z.y = 3.0 * (vUv.y - .5);
+    
+    int y = 0;
+    for (int i = 0; i < 100; i++) {
+        y++;
+        float x = (z.x * z.x - z.y * z.y) + start.x;
+        float y = (z.x * z.y + z.x * z.y) + start.y;
+        
+        if ((x * x + y * y) > 10.0) {
+            break;
+        }
+        z.x = x;
+        z.y = y;
+    }
+    
+    float val = (float(y) / float(iter)) * 0.25;
+    gl_FragColor = vec4(0.5 + val, 0.5 - val, 1.0, 1.0);
+}
+`,
+    'fragment',
+    'three'
+  );
+
+const makeJuliaV = (id, nextStageNodeId) =>
+  core.sourceNode(
+    id,
+    'Julia',
+    {},
+    {
+      version: 2,
+      preprocess: true,
+      strategies: [core.uniformStrategy()],
+      uniforms: [],
+    },
+    `precision highp float;
+    precision highp int;
+    
+    uniform mat4 modelViewMatrix;
+    uniform mat4 projectionMatrix;
+    varying vec2 vUv;
+    
+    attribute vec3 position;
+    attribute vec2 uv;
+    
+    void main() {
+        vUv = uv;
+        gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+    }`,
+    'vertex',
+    'three',
+    nextStageNodeId
+  );
+
+const fId = makeId();
+const juliaF = makeJuliaF(fId);
+const juliaV = makeJuliaV(makeId(), fId);
+
+/**
+ * Build the graph
+ */
 const graph = {
-  nodes: [outputF, outputV, physicalF, physicalV],
+  nodes: [juliaF, juliaV, physicalF, physicalV, outputF, outputV],
   edges: [
+    edgeFrom(juliaF, physicalF.id, 'property_normalMap', 'fragment'),
     edgeFrom(physicalF, outputF.id, 'filler_frogFragOut', 'fragment'),
     edgeFrom(physicalV, outputV.id, 'filler_gl_Position', 'vertex'),
   ],
 };
 
+/**
+ * Compile the graph to GLSL
+ */
 core.compileSource(graph, engine, ctx).then((compileResult) => {
-  console.log('compile result', compileResult);
-
+  /**
+   * Convert the compiled GLSL to a RawShaderMaterial
+   */
   const material = createMaterial(compileResult, ctx);
 
-  material.uniforms = { ...material.uniforms, ...{} };
+  material.uniforms = {
+    ...material.uniforms,
+    ...{
+      [`start_${fId}`]: { value: new THREE.Vector2(-0.2307, 0.6923) },
+      [`iter_${fId}`]: { value: 8 },
+      [`fractal_color_${fId}`]: { value: new THREE.Color(0.0, 0.9, 0.1) },
+    },
+  };
   material.roughness = 0;
 
-  console.log('🏞 Re-creating three.js material!', {
+  console.log('🏞 Re-created Three.js material!', {
     material,
-    uniforms: material.uniforms,
+    compileResult,
   });
 
   mesh.material = material;
-});
 
-function animate() {
-  requestAnimationFrame(animate);
-  controls.update();
-  mesh.rotation.x += 0.01;
-  mesh.rotation.y += 0.01;
-  renderer.render(scene, camera);
-}
-animate();
+  document.getElementById('fragment').value = compileResult.fragmentResult;
+  document.getElementById('vertex').value = compileResult.vertexResult;
+});
